8a86bdd867e36042185e348d39abb9027905fd4a
sumitpriye <sumitsinha125@gmail.com>
2023-02-06 18:50:30 +0530

    added prediction.py

---
+++ prediction_service/prediction.py
@@ -0,0 +1,35 @@
+import yaml
+import os
+import json
+import joblib
+import numpy as np
+
+
+
+
+
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    prediction = model.predict(data)
+    print(prediction)
+    return prediction[0]
+
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response":response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again"}
+        return error
